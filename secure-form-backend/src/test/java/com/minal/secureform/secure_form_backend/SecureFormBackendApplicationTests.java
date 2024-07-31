8d812aa4d4a05f174c44160a7ab83eb679def158
minalrahman <minal3601@gmail.com>
2024-07-30 21:34:11 -0400

    setting up spring backend

---
+++ secure-form-backend/src/test/java/com/minal/secureform/secure_form_backend/SecureFormBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minal.secureform.secure_form_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureFormBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
